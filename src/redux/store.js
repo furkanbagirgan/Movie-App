91d6a85f48829fd42cd7779d93fae5771e9c51f0
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-08 14:55:24 +0300

    add comment line to store, auth and theme slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
-import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
 
 import AuthReducer from './authSlice';
 import ThemeReducer from './themeSlice';
 
+//A store is created by pulling reducers from all slices.
 const store = configureStore({
   reducer: {
     'auth': AuthReducer,
--- src/redux/themeSlice.js
+++ src/redux/themeSlice.js
@@ -1,11 +1,13 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+//This is the slice where theme operations are made.
 const themeSlice = createSlice({
   name: 'theme',
   initialState: {
     theme: 'light',
   },
   reducers: {
+    //The new incoming data is assigned to the existing theme.
     setTheme: (state,action) => {
       return {
         theme: action.payload,
