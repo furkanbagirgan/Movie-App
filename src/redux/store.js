4209fc4bb4f61323bb364957fc38b8a9378642f6
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-09 20:50:20 +0300

    edit search slice and store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,13 +3,15 @@ import {configureStore} from '@reduxjs/toolkit';
 import AuthReducer from './authSlice';
 import ThemeReducer from './themeSlice';
 import MovieReducer from './movieSlice';
+import SearchReducer from './searchSlice';
 
 //A store is created by pulling reducers from all slices.
 const store = configureStore({
   reducer: {
     'auth': AuthReducer,
     'theme': ThemeReducer,
-    'movie': MovieReducer
+    'movie': MovieReducer,
+    'search': SearchReducer
   },
 });
 
